3baa2a8e388d9659531a5f3ab4956413cdf606ac
adamrisaldi <32086322+adamrisaldi@users.noreply.github.com>
2023-12-07 16:47:32 +0700

    Update soal1.js

--- Day8/soal1/soal1.js
+++ Day8/soal1/soal1.js
@@ -11,6 +11,10 @@ let result
         result = true;
     } else if (nums[0] + nums[1] + nums[2] === 4 && nums[0] === 1 && nums[1] === 1) {
         result = true;
+    } else if (nums[0] + nums[1] + nums[3] === 4 && nums[0] === 1 && nums[1] === 2) {
+        result = true;
+    } else if (nums[0] + nums[2] + nums[3] === 4 && nums[0] === 2 && nums[2] === 1) {
+        result = true;
     } else {
         result = false;
     }
