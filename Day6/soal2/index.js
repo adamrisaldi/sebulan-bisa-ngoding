3a102aa3c2f11f439191f0c18cef0242235337ff
adamrisaldi <32086322+adamrisaldi@users.noreply.github.com>
2023-12-08 10:17:38 +0700

    Update index.js

--- Day6/soal2/index.js
+++ Day6/soal2/index.js
@@ -3,16 +3,12 @@
 // console.log(words[1]);
 
 
-
 let kata = 'AntCollab';
 let jumlahKata = kata.length;
 
-let e = '' // bikin ember sebelum loping buat nampung kata yang diambil
+let e = ''
 
-// bikin loop yg membuat jumlah kata berjalan dari belakang
-// setiap looping berjalan, jumlah kata berkurang satu agar huruf yg diambil adalah huruf sebelumnya
 for (let i = 0; jumlahKata > i; jumlahKata--) {
-    // setiap kali memulai looping, menambah huruf terakhir
     e = e + kata[jumlahKata-1]
 }
 console.log(e);
